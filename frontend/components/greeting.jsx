9fd3551cddcdf47d67cae188fbba2696e6e928f3
Zack Yang <zackyang@gmail.com>
2017-06-19 18:08:36 -0400

    initial header styling

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { logout } from '../actions/session_actions';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
+
 
 class Greeting extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.handleLogout = this.handleLogout.bind(this);
   }
 
@@ -17,21 +18,31 @@ class Greeting extends React.Component {
 
   render() {
     const user = this.props.user;
-
+    // How do I actually route myself from my greeting page to my board page in the best way?
     if (user) {
       return (
-        <section>
-          {`Welcome: ${user.username}`}
-          <button onClick={this.handleLogout}>Logout</button>
-        </section>
+        // <section className="landingContainer">
+        //   <div className="landing">
+        <div>
+            <Redirect to="/home" />
+            {`Welcome: ${user.username}`}
+            <button onClick={this.handleLogout}>Logout</button>
+        </div>
+        // </section>
       );
     } else {
       return (
-        <section>
-          <ul>
-            <li><Link to='/signup'>Sign Up</Link></li>
-            <li><Link to='/login'>Log In</Link></li>
-          </ul>
+        <section className="landing-container">
+          <div className="landing-header">
+                      <img className="landing-icon" src="http://media.istockphoto.com/vectors/lightning-icon-red-circle-on-gray-background-vector-id493855699?k=6&m=493855699&s=612x612&w=0&h=8AZ-L-6kVpuVdoFblBH49c_rhs9xpS1GRqMfZI7Jquw=" />
+            Welcome to QuickBoard!
+            <Link className="landing-sign-up-bar" to='/signup'>Sign Up</Link>
+            <Link className="landing-sign-up-bar" to='/login'>Log In</Link>
+          </div>
+
+          <div className="">
+            This is the body
+          </div>
         </section>
       );
     }
