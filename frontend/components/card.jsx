b1fffcd444b53934f9842819cedb91e3052510d9
Zack Yang <zackyang@gmail.com>
2017-06-25 18:50:28 -0400

    refactoring into card component in preparation for react drag and drop

---
+++ frontend/components/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { values, merge } from 'lodash';
+
+import PropTypes from 'prop-types';
+import { DragSource, DragDropContextProvider, DropTarget } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+
+class Card extends React.Component{
+  constructor(props){
+    super(props);
+
+  }
+
+  
+
+}
--- frontend/components/list.jsx
+++ frontend/components/list.jsx
@@ -2,6 +2,11 @@
 import React from 'react';
 import { values, merge } from 'lodash';
 
+import PropTypes from 'prop-types';
+import { DragSource, DragDropContextProvider, DropTarget } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+
 class List extends React.Component{
   constructor(props){
     super(props);
