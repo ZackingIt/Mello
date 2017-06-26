de11339b37421eab0386c4a42af74ed8a9d97064
Zack Yang <zackyang@gmail.com>
2017-06-26 17:31:25 -0400

    removed babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
             'babel-preset-es2015',
             'babel-preset-react',
             'babel-preset-stage-0',
-          ].map(require.resolve),
+          ],
         },
         // query: {
         //   cacheDirectory: true,
