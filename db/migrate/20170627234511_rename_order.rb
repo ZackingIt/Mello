29a8e072b60fd2e0b12e30fdb76acd9e3bdac560
Zack Yang <zackyang@gmail.com>
2017-06-28 05:48:32 -0400

    implementing clickaway

--- db/migrate/20170627234511_rename_order.rb
+++ db/migrate/20170627234511_rename_order.rb
@@ -1,8 +1,8 @@
 class RenameOrder < ActiveRecord::Migration[5.0]
   def change
 
-    rename_column :cards, :ord, :ord
-    rename_column :lists, :ord, :ord
+    rename_column :cards, :order, :ord
+    rename_column :lists, :order, :ord
 
   end
 end
