edde7cbc278c19b13b985ce8d80b07a04f1d5175
Zack Yang <zackyang@gmail.com>
2017-07-14 18:00:28 -0400

    REVISED WEBPACK

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,50 @@
-const path = require('path');
+// webpack.config.js
+var path = require("path");
+var webpack = require("webpack");
 
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+// include plugins config
 module.exports = {
   context: __dirname,
-  entry: './frontend/entry.jsx',
+  entry: "./frontend/entry.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
+  plugins: plugins,
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: [
-            'babel-preset-es2015',
-            'babel-preset-react'
-          ].map(require.resolve),
-        },
-        // query: {
-        //   cacheDirectory: true,
-        //   plugins: ['transform-decorators-legacy' ],
-        //   presets: ['es2015', 'stage-0', 'react']
+          presets: ['react', 'es2015']
         }
+      }
     ]
   },
-  devtool: 'source-maps'
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
