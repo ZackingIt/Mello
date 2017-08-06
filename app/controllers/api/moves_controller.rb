78dd84f5e2e79da4fd1d4ba33ca54bf95364dee0
Zack Yang <zackyang@gmail.com>
2017-08-06 01:39:27 -0400

    undefined api cards route

--- app/controllers/api/moves_controller.rb
+++ app/controllers/api/moves_controller.rb
@@ -7,11 +7,11 @@ class Api::MovesController < ApplicationController
       old_list = @card.list
       new_cards = old_list.cards.sort_by{|card| card.ord}
 
-      # need to revise this into a batch process
+      # need to revise this into a batch process -- this is enforcing 0 to list-length -1 ordering.
       i = 0
       while i < new_cards.length
         new_cards[i].update(ord: i)
-        i+=1
+        i += 1
       end
       old_list_id = @card.list_id
       old_card_order = @card.ord
@@ -19,17 +19,25 @@ class Api::MovesController < ApplicationController
       new_list_id = params[:cardLoad][:ending][:listId].to_i
       new_card_order = params[:cardLoad][:ending][:cardIndex].to_i
       starting_list_id = params[:cardLoad][:starting][:listId].to_i
-      increment_order = []
-      decrement_order = []
 
-      increment_order = Card.where(["list_id = ? AND ord > ? AND id <> ?", new_list_id, new_card_order, @card.id])
-                            .sort_by{|card| card.ord}
+      cards_inc_order_hash = Hash.new{ |h, k| h[k] = {} }
 
-      decrement_order = Card.where(["list_id = ? AND ord <= ? AND id <> ?", new_list_id, new_card_order, @card.id])
-                            .sort_by{|card| card.ord}
+      cards_inc_order = Card.where(["list_id = ? AND ord > ? AND id <> ?", new_list_id, new_card_order, @card.id])
+                            # .sort_by{|card| card.ord}
+      cards_dec_order = Card.where(["list_id = ? AND ord <= ? AND id <> ?", new_list_id, new_card_order, @card.id])
+      
+      cards_inc_order.each do |card_ar_object|
+        cards_inc_order_hash[card_ar_object.id] = { ord: (card_ar_object.ord + cards_dec_order.length) }
+      end
 
-      decrement_order.each_with_index { |el, idx| el.update(list_id: new_list_id, ord: idx) }
-      increment_order.each_with_index { |el, idx| el.update(list_id: new_list_id, ord: (idx + decrement_order.length)) }
+      # cards_dec_order.update_all(list_id: new_list_id)
+      cards_inc_order.update_all(cards_inc_order_hash)
+                            # .sort_by{|card| card.ord}
+
+
+
+      # cards_dec_order.each_with_index { |el, idx| el.update(list_id: new_list_id, ord: idx) }
+      # cards_inc_order.each_with_index { |el, idx| el.update(list_id: new_list_id, ord: (idx + cards_dec_order.length)) }
 
       @card.update(list_id: new_list_id, ord: new_card_order )
 
